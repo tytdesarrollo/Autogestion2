833a530b901e97a8b5d935931f37d3b76054371c
vlaxb <vladix88@gmail.com>
2016-06-10 15:58:46 -0500

    alert en index
    agrego el alert info en el index

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
+	'language' => 'es',
     'bootstrap' => ['log'],
     'components' => [
         'request' => [
