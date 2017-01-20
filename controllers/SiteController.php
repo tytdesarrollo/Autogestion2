5af6bb930cafb05aeb438d0948f3d936b1d53035
Hom0824 <hom-0824@hotmail.com>
2017-01-20 17:23:33 -0500

    Revert "Ajustes visuales"
    This reverts commit 6639b54dae059e2268788ebe023c7d0229c8e307.

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -25,13 +25,11 @@ class SiteController extends Controller
 
 	public function actionSaluda(){
 
-		// $model = new TwPcIdentity;
+		$model = new TwPcIdentity;
 
-		// $proce = $model->procedimiento();
-		$proce = 'Hello';
-		$price = 'world';
+		$proce = $model->procedimiento();
 
-        return $this->render('saluda', ["rows"=>$proce,"rows2"=>$price]);
+        return $this->render('saluda', ["rows"=>$proce]);
 	}		
 	
     public function behaviors()
@@ -234,143 +232,6 @@ class SiteController extends Controller
                 return $this->render('vacaciones',['events' => $events]);
 			}        
 		
-    }
-	
-	public function actionTurnos()
-    {
-		$tablet_browser = 0;
-			$mobile_browser = 0;
-			$body_class = 'desktop';
-			 
-			if (preg_match('/(tablet|ipad|playbook)|(android(?!.*(mobi|opera mini)))/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
-				$tablet_browser++;
-				$body_class = "tablet";
-			}
-			 
-			if (preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|android|iemobile)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
-				$mobile_browser++;
-				$body_class = "mobile";
-			}
-			 
-			if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml') > 0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
-				$mobile_browser++;
-				$body_class = "mobile";
-			}
-			 
-			$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
-			$mobile_agents = array(
-				'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
-				'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
-				'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
-				'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
-				'newt','noki','palm','pana','pant','phil','play','port','prox',
-				'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
-				'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
-				'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
-				'wapr','webc','winw','winw','xda ','xda-');
-			 
-			if (in_array($mobile_ua,$mobile_agents)) {
-				$mobile_browser++;
-			}
-			 
-			if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'opera mini') > 0) {
-				$mobile_browser++;
-				//Check for tablets on opera mini alternative headers
-				$stock_ua = strtolower(isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA'])?$_SERVER['HTTP_X_OPERAMINI_PHONE_UA']:(isset($_SERVER['HTTP_DEVICE_STOCK_UA'])?$_SERVER['HTTP_DEVICE_STOCK_UA']:''));
-				if (preg_match('/(tablet|ipad|playbook)|(android(?!.*mobile))/i', $stock_ua)) {
-				  $tablet_browser++;
-				}
-			}
-			if ($tablet_browser > 0) {
-			// Si es tablet has lo que necesites
-			$events = Yii::$app->mysqldb->createCommand("SELECT ID AS ID, TITLE AS TITLE, START AS START, END AS END, COLOR AS COLOR FROM EVENTS")->queryAll();
-			
-			$this->view->params['customParam'] = $events;
-
-                return $this->render('mturnos',['events' => $events]);
-
-			}
-			else if ($mobile_browser > 0) {
-			// Si es dispositivo mobil has lo que necesites			
-			   $events = Yii::$app->mysqldb->createCommand("SELECT ID AS ID, TITLE AS TITLE, START AS START, END AS END, COLOR AS COLOR FROM EVENTS")->queryAll();
-			
-			$this->view->params['customParam'] = $events;
-
-                return $this->render('mturnos',['events' => $events]);
-			}
-			else {
-			// Si es ordenador de escritorio has lo que necesites
-			$events = Yii::$app->mysqldb->createCommand("SELECT ID AS ID, TITLE AS TITLE, START AS START, END AS END, COLOR AS COLOR FROM EVENTS")->queryAll();
-			
-			$this->view->params['customParam'] = $events;
-				
-                return $this->render('turnos',['events' => $events]);
-			}        
-		
-    }
-	
-	public function actionMturnos()
-    {
-				if (Yii::$app->user->isGuest) {
-			 return $this->goBack();
-        }else{
-		
-		$tablet_browser = 0;
-			$mobile_browser = 0;
-			$body_class = 'desktop';
-			 
-			if (preg_match('/(tablet|ipad|playbook)|(android(?!.*(mobi|opera mini)))/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
-				$tablet_browser++;
-				$body_class = "tablet";
-			}
-			 
-			if (preg_match('/(up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone|android|iemobile)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
-				$mobile_browser++;
-				$body_class = "mobile";
-			}
-			 
-			if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']),'application/vnd.wap.xhtml+xml') > 0) or ((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
-				$mobile_browser++;
-				$body_class = "mobile";
-			}
-			 
-			$mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
-			$mobile_agents = array(
-				'w3c ','acs-','alav','alca','amoi','audi','avan','benq','bird','blac',
-				'blaz','brew','cell','cldc','cmd-','dang','doco','eric','hipt','inno',
-				'ipaq','java','jigs','kddi','keji','leno','lg-c','lg-d','lg-g','lge-',
-				'maui','maxo','midp','mits','mmef','mobi','mot-','moto','mwbp','nec-',
-				'newt','noki','palm','pana','pant','phil','play','port','prox',
-				'qwap','sage','sams','sany','sch-','sec-','send','seri','sgh-','shar',
-				'sie-','siem','smal','smar','sony','sph-','symb','t-mo','teli','tim-',
-				'tosh','tsm-','upg1','upsi','vk-v','voda','wap-','wapa','wapi','wapp',
-				'wapr','webc','winw','winw','xda ','xda-');
-			 
-			if (in_array($mobile_ua,$mobile_agents)) {
-				$mobile_browser++;
-			}
-			 
-			if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']),'opera mini') > 0) {
-				$mobile_browser++;
-				//Check for tablets on opera mini alternative headers
-				$stock_ua = strtolower(isset($_SERVER['HTTP_X_OPERAMINI_PHONE_UA'])?$_SERVER['HTTP_X_OPERAMINI_PHONE_UA']:(isset($_SERVER['HTTP_DEVICE_STOCK_UA'])?$_SERVER['HTTP_DEVICE_STOCK_UA']:''));
-				if (preg_match('/(tablet|ipad|playbook)|(android(?!.*mobile))/i', $stock_ua)) {
-				  $tablet_browser++;
-				}
-			}
-			if ($tablet_browser > 0) {
-			// Si es tablet has lo que necesites
-			   return $this->render('mturnos');
-			}
-			else if ($mobile_browser > 0) {
-			// Si es dispositivo mobil has lo que necesites
-			   return $this->render('mturnos');
-			}
-			else {
-			// Si es ordenador de escritorio has lo que necesites			 
-			  return $this->render('turnos');
-			}
-		}
     }
 	
 	public function actionAddevent(){
