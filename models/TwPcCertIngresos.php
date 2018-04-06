2e47f7c53efe2899057df9008f3ab91295bdb908
vlaxb <vladix88@gmail.com>
2018-04-06 13:01:57 -0500

    actualizacion conexion oci + utf8
    actualizacion conexion oci + utf8 + params

--- models/TwPcCertIngresos.php
+++ models/TwPcCertIngresos.php
@@ -13,27 +13,33 @@ class TwPcCertIngresos extends Model{
     public function procedimiento()
     {
 		
-	$cod_epl = Yii::$app->session['cedula'];
-	$ano = Yii::$app->session['ano'];
+		$CODIGO_EPL = Yii::$app->session['cedula'];
+		$ANO_FORMU = Yii::$app->session['ano'];
+	//Strings
+		$BLOQUE1;		
+		$BLOQUE2;	
+		$OUTPUT;	
+		$MESSAGE;	
 
-  $CODIGO_EPL= $cod_epl;
-  $ANO_FORMU= $ano;
-  $BLOQUE1= '';
-  $BLOQUE2= '';
+      	// TNS DE LA BASE DE DATOS
+    	$db = Yii::$app->params['orcl'];		
+		$usr = Yii::$app->params['usr'];		
+		$psw = Yii::$app->params['psw'];	
+		//conexion con la base de datos
+		$conexion = oci_connect($usr, $psw, $db);
+		//procedimiento a ejecutar
+		$stid = oci_parse($conexion, "BEGIN TW_PC_CERT_INGRESOS1(:CODIGO_EPL,:ANO_FORMU,:BLOQUE1,:BLOQUE2,:OUTPUT,:MESSAGE); END;");
 
-  
-		$rows = Yii::$app->telmovil->createCommand("BEGIN TW_PC_CERT_INGRESOS1 (:CODIGO_EPL,:ANO_FORMU,:BLOQUE1,:BLOQUE2,:OUTPUT,:MESSAGE);	END;");
-
-$rows->bindParam(":CODIGO_EPL", $CODIGO_EPL, PDO::PARAM_STR);
-$rows->bindParam(":ANO_FORMU", $ANO_FORMU, PDO::PARAM_STR);
-$rows->bindParam(":BLOQUE1", $BLOQUE1, PDO::PARAM_STR,1000);
-$rows->bindParam(":BLOQUE2", $BLOQUE2, PDO::PARAM_STR,1000);
-$rows->bindParam(":OUTPUT", $BLOQUE3, PDO::PARAM_STR,1000);
-$rows->bindParam(":MESSAGE", $BLOQUE4, PDO::PARAM_STR,1000);
-
-$rows->execute();
-
-return $twpccertingresos = array($BLOQUE1,$BLOQUE2);
+		oci_bind_by_name($stid, ':CODIGO_EPL', $CODIGO_EPL, 50);
+		oci_bind_by_name($stid, ':ANO_FORMU', $ANO_FORMU, 50);
+		oci_bind_by_name($stid, ':BLOQUE1', $BLOQUE1, 1000);
+		oci_bind_by_name($stid, ':BLOQUE2', $BLOQUE2, 1000);
+		oci_bind_by_name($stid, ':OUTPUT', $OUTPUT, 1000);
+		oci_bind_by_name($stid, ':MESSAGE', $MESSAGE, 1000);
+		//
+		oci_execute($stid);
+		//
+		return $twpccertingresos = array($BLOQUE1,$BLOQUE2);
 
 	}	
 }
